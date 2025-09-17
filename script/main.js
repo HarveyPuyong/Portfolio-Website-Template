012aa2d7a81d094e1333d1acde0f49986a223a9d
John Harvey <harveyluslos80@example.com>
2025-09-17 15:43:43 +0800

    feat(animation): add onscroll animations for all sections using GSAP feat(animation): enable start animation only on large screen view

--- script/main.js
+++ script/main.js
@@ -1,27 +1,92 @@
-const sections = document.querySelectorAll("section"); // all sections
-const navLinks = document.querySelectorAll(".navigation-list__icon"); // nav items
+// ================================
+//  NAVIGATION OBSERVER
+// ================================
+const observeActiveNav = () => {
+  const sections = document.querySelectorAll("section"); 
+  const navLinks = document.querySelectorAll(".navigation-list__icon"); 
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      const id = entry.target.getAttribute("id");
-      const navItem = document.querySelector(`.navigation-list a[href="#${id}"]`);
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        const navItem = document.querySelector(`.navigation-list a[href="#${id}"]`);
 
-      if (entry.isIntersecting) {
-        console.log(entry.target.id)
-        // remove active from all
-        navLinks.forEach((link) => link.classList.remove("active"));
-        // add active to current
-        navItem.classList.add("active");
+        if (entry.isIntersecting) {
+          navLinks.forEach((link) => link.classList.remove("active"));
+          navItem.classList.add("active");
+        }
+      });
+    },
+    {
+      root: null,
+      threshold: 0.1, 
+    }
+  );
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+}
+
+
+// ================================
+//  ON-SCROLL ELEMENTS ANIMATION
+// ================================
+const onscrollElementsAnimation = () => {
+   gsap.registerPlugin(ScrollTrigger);
+
+  const scrollerTarget = window.innerWidth > 900 ? ".content-sections" : null;
+  const animationStart = window.innerWidth > 900 ? "top 2700%" : "top 95%";
+
+  // Sections Elements Animation(Except Language Bar)
+  const sectionsElements = document.querySelectorAll(".content-sections section *:not(.language-label):not(.language-bar .bar)");
+
+  sectionsElements.forEach(element => {
+    gsap.from(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: animationStart,
+          toggleActions: "play none none reverse", 
+          scroller: scrollerTarget,
+        },
+        opacity: 0,
+        y: 27, 
+        duration: 0.8,
+        ease: "power2.out"
       }
-    });
-  },
-  {
-    root: null, // viewport
-    threshold: 0.1, // % of section visible before marking it active
-  }
-);
+    );
+  });
 
-sections.forEach((section) => {
-  observer.observe(section);
-});
+  //Language Bars Animation
+  const languageBars = document.querySelectorAll('.language-list .language-bar .bar');
+
+  languageBars.forEach(bar => {
+    let targetWidth = bar.getAttribute("data-width"); 
+
+    gsap.fromTo(bar, 
+      { width: 0 },
+      { 
+        width: targetWidth + "%", 
+        duration: 1, 
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: bar,           
+          start: animationStart,      
+          toggleActions: "play none none reverse",
+          scroller: scrollerTarget 
+        }
+      }
+    );
+  });
+}
+
+
+// ================================
+//  MAIN FUNCTION
+// ================================
+function main(){
+  observeActiveNav();
+  onscrollElementsAnimation();
+}
+
+main();
